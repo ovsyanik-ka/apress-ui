a6ec2f14e4c25eb29acfb8e1fa09e430ea653522
kozyrk <vkseniav@gmail.com>
2019-06-24 16:49:50 +0500

    feature(pulscen): add availability-label, popup-hint_static-width and popup-hint_static-width
    https://jira.railsc.ru/browse/PC4-23657

--- src/blocks/pulscen/text-label/index.js
+++ src/blocks/pulscen/text-label/index.js
@@ -1,5 +1,6 @@
 import './text-label.css';
 import './text-label_positive.css';
+import './text-label_negative.css';
 import './text-label_neutral.css';
 import './text-label_highlighted.css';
 import './text-label_support.css';
