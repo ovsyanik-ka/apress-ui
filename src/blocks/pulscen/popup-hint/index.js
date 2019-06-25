4abca9ec637d0ef252c46e46cadbf69b9e03588e
kozyrk <vkseniav@gmail.com>
2019-06-25 11:40:21 +0500

    feature(pulscen): popup-hint modification
    https://jira.railsc.ru/browse/PC4-23657

--- src/blocks/pulscen/popup-hint/index.js
+++ src/blocks/pulscen/popup-hint/index.js
@@ -1,2 +1,4 @@
 import './popup-hint.css';
-import './popup-hint_static-width.css';
+import './popup-hint_width_static.css';
+import './popup-hint_position_right.css'
+import './popup-hint_position_center.css'
