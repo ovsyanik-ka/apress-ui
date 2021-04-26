b62fc8d67c32c29f7c7b28cee5ba68a9ee520465
KonstantinBel <ksb.bart@gmail.com>
2021-04-26 21:52:46 +0300

    chore(module-loader): create module-loader for blizko with new cross domain auth
    [MINOR] создал отдельный модуль module-loader для близки с веткой localstorage

---
+++ src/blocks/blizko/modules-loader/modules-loader.js
@@ -0,0 +1,36 @@
+// TODO: перенести в common поле того как ПЦ перейдет на новую кроссдоменку
+
+(function() {
+  function _loadMainModules(eventName) {
+    var moduleLoader = function () {
+      for (var item in app.modules) {
+        if (!app.modules[item][eventName]) { continue; }
+
+        try {
+          if (app.modules[item].initiated) { return; }
+          app.modules[item][eventName]();
+          app.modules[item].initiated = true;
+        } catch(error) {
+          console.trace(error);
+        }
+      }
+    };
+
+    setTimeout(moduleLoader, 10);
+  }
+
+  function _onWindowLoad() {
+    if (app.config.authIsComplete) {
+      app.config.authIsComplete.then(_loadMainModules.bind(null, 'load'));
+    } else {
+      _loadMainModules('load');
+    }
+  }
+
+  function _listener() {
+    document.addEventListener('DOMContentLoaded', _loadMainModules.bind(null, 'ready'));
+    window.addEventListener('load', _onWindowLoad);
+  }
+
+  app.modules && _listener();
+})();
