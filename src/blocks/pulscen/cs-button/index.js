2c0cb676e4f2756cf5e54388c10950f2faa274f3
DanteDeGrave <vladimir.system@gmail.com>
2022-02-14 15:04:47 +0500

    fix(sc_button): удалит лишние классы [PATCH]: удалит лишние классы https://jira.railsc.ru/browse/PC4-27888

--- src/blocks/pulscen/cs-button/index.js
+++ src/blocks/pulscen/cs-button/index.js
@@ -3,12 +3,5 @@ import './cs_button_medium.css';
 import './cs_button_big.css';
 import './cs_button_small.css';
 import './cs_button_transparent.css';
-import './cs_button_callback.css';
-import './cs_button_callback-big.css';
 import './cs_button_wide.css';
 import './cs_button_thin.css';
-
-import './cs_button_only-icon.css';
-
-import './cs_button_square.css';
-import './cs_button_square-big.css';
