2c28a86516ddf0aad02339f01fe8b0d6e0e15824
Levushkina Marina <nara-yan@bk.ru>
2022-04-07 16:38:12 +0500

    feature(pulscen): add button_transparent-light
    [MINOR]: добавит для ПЦ кнопку button_transparent-light
    
    https://jira.railsc.ru/browse/PC4-27966

--- src/blocks/pulscen/button/index.js
+++ src/blocks/pulscen/button/index.js
@@ -10,6 +10,7 @@ import './button_transparent.css';
 import './button_transparent-grey.css';
 import './button_transparent-red.css';
 import './button_transparent-yellow.css';
+import './button_transparent-light.css';
 
 import './button_gray.css';
 import './button_green.css';
