781541f9aa6b28ed4f8044850bd28cd773062d1f
DanteDeGrave <vladimir.system@gmail.com>
2022-05-06 15:28:40 +0500

    feature(ПульсЦен): добавит link_mail, поправит стили highlighted [MINOR]: добавит link_mail, поправит стили highlighted https://jira.railsc.ru/browse/PC4-28171

--- src/blocks/pulscen/link/index.js
+++ src/blocks/pulscen/link/index.js
@@ -1,11 +1,12 @@
 import 'blocks/common/link';
 import './link.css';
 import './link_gray.css';
-import './link_highlighted.css';
 import './link_dashed.css';
 import './link_callback.css';
 import './link_phone.css';
+import './link_mail.css';
 import './link_blocked.css';
 import './link_dark.css';
 import './link_dotted.css';
 import './link_disabled.css';
+import './link_highlighted.css';
