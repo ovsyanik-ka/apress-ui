31336aecb579877bceb42ee7d225da4830d19256
DanteDeGrave <vladimir.system@gmail.com>
2022-08-22 14:44:40 +0500

    feature(pulscen\cs-button): add only-icon, square and cart buttons
    [MINOR]: add only-icon, square and cart buttons in pulscen\cs-button
    
    https://jira.railsc.ru/browse/PC4-28560

--- src/blocks/pulscen/cs-button/index.js
+++ src/blocks/pulscen/cs-button/index.js
@@ -1,7 +1,9 @@
 import './cs_button.css';
 import './cs_button_medium.css';
 import './cs_button_big.css';
-import './cs_button_small.css';
 import './cs_button_transparent.css';
 import './cs_button_wide.css';
 import './cs_button_thin.css';
+import './cs_button_cart.css';
+import './cs_button_only-icon.css';
+import './cs_button_square.css';
