0d888b6b00936ba5b44924e39bb00209c2c66e8f
Andrey Kloninger <absurdddiam@gmail.com>
2022-11-24 08:43:27 +0500

    feature(blizko): add discount label
    [MINOR]: добавит метку скидки на BLIZKO
    https://jira.railsc.ru/browse/BPC-21369

--- src/blocks/blizko/marketing-label/index.js
+++ src/blocks/blizko/marketing-label/index.js
@@ -2,3 +2,4 @@ import './marketing-label.css';
 import './marketing-label_desktop.css';
 import './marketing-label_mobile.css';
 import './marketing-label_rating.css';
+import './marketing-label_discount.css';
